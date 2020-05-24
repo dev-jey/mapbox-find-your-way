7112cd12d3f773c54d330c952e95b7d4e19fdc36
damibiu1 <lordbiu10@gmail.com>
2020-05-24 18:28:41 +0300

    Add updated gulp file with asynchronous task execution

--- dist/js/output.js
+++ dist/js/output.js
@@ -103,20 +103,27 @@ const cleanCSS = require('gulp-clean-css');
 const babel = require('gulp-babel');
 const sourcemaps = require('gulp-sourcemaps');
 const concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+const terser = require('gulp-terser');
+var all = require('gulp-all')
 
-gulp.task('minify-js', function () {
-    return gulp.src('*.js')
-        .pipe(sourcemaps.init())
-        // .pipe(babel())     
-        .pipe(concat('output.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist/js'))
-});
+function build() {
+    return all(
+        gulp.src('*.js')
+            .pipe(terser())
+            .pipe(gulp.dest('dist/js')),
 
-gulp.task('minify-css', () => {
-    return gulp.src('*.css')
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest('dist/css'));
-});
+        gulp.src('*.js')
+            .pipe(sourcemaps.init())
+            .pipe(concat('output.js'))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest('dist/js'))
+        ,
+        gulp.src('*.css')
+            .pipe(cleanCSS({ compatibility: 'ie8' }))
+            .pipe(gulp.dest('dist/css'))
+    )
+};
 
+gulp.task('default', build);
 //# sourceMappingURL=output.js.map
